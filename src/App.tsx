9ca9c99fb310e077856c797d77a40d330932d0fc
iGrOdAcz <111497013+iGrOdAcz@users.noreply.github.com>
2024-05-13 17:43:25 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useState } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "Ne",
+      "Jsi si jistá?",
+      "Ani když pořádně poprosím?",
+      "Pjosím",
+      "Udělam pro tebe vše",
+      "Dám ti masáž",
+      "PROSÍM POOKIE",
+      "Ale :*(",
+      "Jdu se zabít",
+      "Yep jsem mrtvý",
+      "mluvíš teďka s duchem",
+      "prosím lásko",
+      ":((((",
+      "Pjosím",
+      "Estoy muerto",
+      "Ne :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! Miluji tě pookie!! ;))</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="my-4 text-4xl">Dáme sexík, až přijedeš?</h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Ano
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "Ne" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
